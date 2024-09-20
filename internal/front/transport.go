2509ea5b01bdbfc1bf5644ace1b6a37c392f68ab
Ilya Timofeev <ilya.ufo@gmail.com>
2024-09-20 11:44:20 +0300

    Front rest server stub

--- internal/front/transport.go
+++ internal/front/transport.go
@@ -1,6 +1,9 @@
 package front
 
 import (
+	"context"
+	"encoding/json"
+	"errors"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -16,4 +19,25 @@ func (sf *Front) getFileHandler(resp http.ResponseWriter, req *http.Request) {
 	fileID := chi.URLParam(req, "fileID")
 	fileSize := req.URL.Query().Get("fileSize")
 	resp.Write([]byte("fileID: " + fileID + ", fileSize: " + fileSize))
+}
+
+func (sf *Front) error(_ *http.Request, w http.ResponseWriter, err error) {
+	switch {
+	case errors.Is(err, context.Canceled):
+		writeErrResponse(w, "timeout", http.StatusRequestTimeout)
+	default:
+		writeErrResponse(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func writeErrResponse(w http.ResponseWriter, err string, status int) {
+	w.Header().Set("Content-type", "application/json")
+
+	w.WriteHeader(status)
+
+	m := map[string]interface{}{
+		"error": err,
+		"code":  status,
+	}
+	_ = json.NewEncoder(w).Encode(m)
 }
